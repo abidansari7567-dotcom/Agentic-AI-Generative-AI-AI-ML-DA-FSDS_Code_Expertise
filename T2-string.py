0b820d07ca491aa67f52da3cb94173ef0105e9a8
Abid Ansari <abidansari7567@gmail.com>
2026-01-09 22:31:10 +0530

    Add files via upload

---
+++ T2-string.py
@@ -0,0 +1,82 @@
+letter = 'p'
+print(letter)
+print(len(letter))
+greeting = 'Hello, world'
+print(greeting)
+print(len(greeting))
+sentence = "i hope you are enjoy 30 days of python challenge"
+print(sentence)
+
+
+multiline_string = '''i am student and enjoy study'''
+print(multiline_string)
+multiline_string = """I am teacher and enjoy teaching"""
+print(multiline_string)
+
+
+first_name = 'Abid'
+last_name = 'Ansari'
+space = '  '
+full_name = first_name + space + last_name 
+print(full_name)
+
+print(len(first_name))
+print(len(last_name))
+print(len(space))
+print(len(full_name))
+
+
+language = 'python'
+a, b, c, d, e = language
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+language = 'python'
+first_letter = language[0]
+print(first_letter)
+last_letter = language[-1]
+print(last_letter)
+second_last = language[-2]
+print(second_last)
+
+language = 'python'
+first_three = language[0:3]
+last_three = language[3:6]
+print(last_three)
+
+last_three = language[-3:]
+print(last_three)
+last_three = language[3:]
+print(last_three)
+
+print('I hope every one enjoy the python challenge.\nDo you?')
+print('days\topic\exercise')
+print('day 1\t3\t5')
+print('day 2\t3\t5')
+print('day 3\t3\t5')
+
+
+challenge = 'thirty day of python'
+print(challenge.capitalize())
+
+challenge = 'thirty days of python'
+print(challenge.count('y'))
+print(challenge.count('y', 6, 7, 9))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
