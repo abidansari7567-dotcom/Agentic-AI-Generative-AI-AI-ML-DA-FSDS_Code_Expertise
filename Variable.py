202628dc11ea1cb945433172663d9651f873cb58
Abid Ansari <abidansari7567@gmail.com>
2026-01-10 19:38:28 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,34 @@
+first_name = 'Abid'
+last_name = 'Ansari'
+country = 'Hyd'
+city = 'Telengana'
+age = 25
+is_married = False
+Skill = ['Python', 'numpy', 'pandas', 'sql',]
+person_info = {
+    'firstname':'Asaben',
+    'lastname':'yatech',
+    'country':'fineland',
+    'city':'Helsinki'
+}
+
+
+print('first name:', first_name)
+print('first name len:', len(first_name))
+print('last_name:', last_name)
+print('last name len:  ', last_name)
+print('country: ', country)
+print('city: ', city)
+print('age: ', age)
+print('married ', is_married)
+print('skill: ', Skill)
+print('person information: ', person_info)
+
+first_name, last_name, country, age, is_married = 'Abid', 'Ansari', 'Helsink', 25, False
+
+print(first_name, last_name, country, age, is_married)
+print('first name:', first_name)
+print('last name: ', last_name)
+print('country: ', country)
+print('age: ', age)
+print('married: ', is_married)
